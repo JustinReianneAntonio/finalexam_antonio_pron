1a0f9ecf28b5b082dd28967d62bca9575d315c08
krystellepron <krystellepron121805@gmail.com>
2026-05-21 10:02:18 +0800

    index blade,migrations,controller

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 use App\Models\employee;
 
 class employeecontroller extends Controller
+/**
+ * tin mga ineditq 
+ * index blade hindi ko mapagana employees
+ * migrations check mo comment don
+ */
 {
     public function index()
     {   
@@ -16,25 +21,36 @@ class employeecontroller extends Controller
 
     public function create()
     {
-        //
+        return view ('employee.create');
     }
-
-
-    public function store(Request $request)
+        public function store(Request $request)
     {
-    //
+        $employee = new employee;
+        $employee->name = $request->name;
+        $employee->email = $request->email;
+        $employee->phone = $request->phone;
+        $employee->save();
+        return redirect()->route('employee.index');
     }
 
     public function edit( int $id)
     {
-        //
+        $employee = employee::find($id);
+        return view('employee.edit', compact('employee'));
     }
 
     public function update(Request $request, int $id) {
-        //
+        $employee = employee::find($id);
+        $employee->name = $request->name;
+        $employee->email = $request->email;
+        $employee->phone = $request->phone;
+        $employee->save();
+        return redirect()->route('employee.index');
     }
 
-    public function destroy(int $id){
-      //
+    public function destroy(int $id){  
+        $employee = employee::find($id);
+        $employee->delete();
+        return redirect()->route('employee.index');
     }
 }
---
+++ database/migrations/2026_05_21_015926_employeemngt.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /**Schema::create('employeemngt', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('mname');
+            $table->string('lname');
+            $table->string('address');
+            $table->string('dob');
+            $table->string('contact');
+            $table->timestamps();
+        });
+        */
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //Schema::dropIfExists('employeemngt');
+        //check mo nalang mga comment tin di kasi ako sure ty!
+    }
+};
